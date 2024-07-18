1c6bccd3c069d08954b2974189f2baa5a04abdcd
Dnevend <Dnevend@gmail.com>
2024-07-18 21:07:53 +0800

    chore: text

--- src/components/OperateBar.tsx
+++ src/components/OperateBar.tsx
@@ -24,24 +24,24 @@ export const OperateBar = ({
     <div className="flex flex-wrap justify-center gap-2">
       <Button size="sm" onClick={() => onTakeATurn()}>
         <PenLine className="w-4 h-4 mr-2" />
-        Take a turn
+        描述
       </Button>
 
       <Button size="sm" onClick={() => onContinue()}>
         <Wand className="w-4 h-4 mr-2" />
-        Continue
+        续写
       </Button>
 
       {retryVisible && (
         <Button size="sm" onClick={() => onRetry()}>
           <Repeat className="w-4 h-4 mr-2" />
-          Retry
+          重写
         </Button>
       )}
       {eraseVisible && (
         <Button size="sm" onClick={() => onErase()}>
           <Delete className="w-4 h-4 mr-2" />
-          Erase
+          擦除
         </Button>
       )}
       {generateNFTVisible && (
