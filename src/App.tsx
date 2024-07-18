1976050d9a10fa80852dedc3dcc59f3092bb9351
Dnevend <Dnevend@gmail.com>
2024-07-18 16:27:42 +0800

    styles: app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import { cn } from "./lib/utils";
 import http from "@/lib/http/axios";
 import { Input } from "./components/ui/input";
-import { Send, PenLine, Wand, Delete, X, FileCheck2 } from "lucide-react";
+import {
+  Send,
+  PenLine,
+  Wand,
+  Delete,
+  X,
+  Repeat,
+  FileCheck2,
+} from "lucide-react";
 import { Button } from "./components/ui/button";
 import { DEFAULT_MODEL } from "./const";
 import { ConnectKitButton } from "connectkit";
 import { IPFS_GATEWAY, pinJSONToIPFS } from "./lib/pinata";
-import { Skeleton } from "./components/ui/skeleton";
 
 interface Message {
   content: string;
@@ -21,7 +28,17 @@ interface ModelResponse {
   model: string;
 }
 
+type TurnType = "do" | "say" | "story" | "see";
+
+const TurnPlaceholder: Record<TurnType, string> = {
+  do: "What do you do?",
+  say: "What do you say?",
+  story: "What happens next?",
+  see: "What do you see?",
+};
+
 function App() {
+  const [turnType, setTurnType] = useState<TurnType>("do");
   const [fetching, setFetching] = useState<boolean>(false);
   const [writing, setWriting] = useState<boolean>(false);
   const inputRef = useRef<HTMLInputElement>(null);
@@ -41,7 +58,7 @@ function App() {
       );
 
       if (data.choices.length > 0) {
-        setMessages((msg) => [...msg, data.choices[0].message]);
+        setMessages([...messages, data.choices[0].message]);
       }
     } finally {
       setFetching(false);
@@ -63,7 +80,9 @@ function App() {
   };
 
   return (
-    <div className={cn("relative flex flex-col min-h-screen overflow-hidden")}>
+    <div
+      className={cn("dark relative flex flex-col min-h-screen overflow-hidden")}
+    >
       <div className="fixed left-0 top-0 -z-10 h-full w-full">
         <div className="absolute top-0 z-[-2] min-h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
       </div>
@@ -75,7 +94,7 @@ function App() {
           "fixed top-0 w-full flex justify-between items-center p-6 px-4"
         )}
       >
-        <div className="text-white text-xl font-bold">丛生 · Cluster</div>
+        <div className={cn("text-white text-xl font-bold")}>丛生 · Cluster</div>
         <ConnectKitButton />
       </header>
 
@@ -90,83 +109,107 @@ function App() {
               );
             }
           })}
-
-          {fetching && (
-            <div className="flex flex-col gap-2">
-              <Skeleton className="h-4 w-[92%] self-end" />
-              <Skeleton className="h-4 w-full" />
-              <Skeleton className="h-4 w-[92%]" />
-            </div>
-          )}
         </div>
       </main>
 
-      <section className="w-full max-w-screen-md mx-auto p-4">
-        {!writing && (
-          <div className="flex justify-between">
-            <div className="flex space-x-2">
-              <Button onClick={() => setWriting(true)}>
-                <PenLine className="w-4 h-4 mr-2" />
-                Take a turn
-              </Button>
-              {messages.length > 0 &&
-                messages[messages.length]?.role === "assistant" && (
-                  <Button
-                    onClick={() => {
-                      // TODO:
-                    }}
-                  >
-                    <Wand className="w-4 h-4 mr-2" />
-                    Retry
-                  </Button>
-                )}
-              {messages.length > 0 &&
-                messages[messages.length]?.role === "assistant" && (
-                  <Button
-                    onClick={() => {
-                      // setMessages(messages.)
-                    }}
-                  >
-                    <Delete className="w-4 h-4 mr-2" />
-                    Erase
-                  </Button>
-                )}
-            </div>
-
-            <Button onClick={() => generateNFT()}>
-              Done
-              <FileCheck2 />
+      <section className="w-full max-w-screen-md mx-auto p-6">
+        {fetching && (
+          <div className="flex space-x-2 justify-center items-center">
+            <span className="sr-only">Loading...</span>
+            <div className="h-4 w-4 bg-white rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+            <div className="h-4 w-4 bg-white rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+            <div className="h-4 w-4 bg-white rounded-full animate-bounce"></div>
+          </div>
+        )}
+        {!fetching && !writing && (
+          <div className="flex justify-center space-x-2">
+            <Button size="sm" onClick={() => setWriting(true)}>
+              <PenLine className="w-4 h-4 mr-2" />
+              Take a turn
             </Button>
+            <Button
+              size="sm"
+              onClick={() => {
+                sendMessage([...messages, { role: "user", content: "继续" }]);
+              }}
+            >
+              <Wand className="w-4 h-4 mr-2" />
+              Continue
+            </Button>
+            {messages[messages.length - 1]?.role === "assistant" && (
+              <Button
+                size="sm"
+                onClick={async () => {
+                  const _messages = [...messages];
+                  console.log("🐞 => onClick={ => _messages:", _messages);
+                  _messages.splice(-1);
+                  console.log("🐞 => onClick={ => _messages:", _messages);
+                  setMessages(_messages);
+                  await sendMessage(_messages);
+                }}
+              >
+                <Repeat className="w-4 h-4 mr-2" />
+                Retry
+              </Button>
+            )}
+            {messages[messages.length - 1]?.role === "assistant" && (
+              <Button size="sm" onClick={() => {}}>
+                <Delete className="w-4 h-4 mr-2" />
+                Erase
+              </Button>
+            )}
+            {messages.length >= 2 && (
+              <Button
+                size="sm"
+                variant="secondary"
+                onClick={() => generateNFT()}
+              >
+                <FileCheck2 />
+              </Button>
+            )}
           </div>
         )}
 
-        {writing && (
+        {!fetching && writing && (
           <div className="rounded-lg border bg-white bg-opacity-10 border-white p-2 mt-2">
             <div className="flex space-x-2">
-              <Button size="icon" onClick={() => setWriting(false)}>
+              <Button
+                size="sm"
+                className="rounded-full"
+                onClick={() => setWriting(false)}
+              >
                 <X />
               </Button>
-              <Button>Do</Button>
-              <Button>Say</Button>
-              <Button>Story</Button>
-              <Button>See</Button>
+              <Button size="sm" onClick={() => setTurnType("do")}>
+                Do
+              </Button>
+              <Button size="sm" onClick={() => setTurnType("say")}>
+                Say
+              </Button>
+              <Button size="sm" onClick={() => setTurnType("story")}>
+                Story
+              </Button>
+              <Button size="sm" onClick={() => setTurnType("see")}>
+                See
+              </Button>
             </div>
 
-            <div className="flex space-x-2 mt-2">
+            <div className="flex items-center space-x-2 mt-2">
               <Input
+                className="text-white"
                 ref={inputRef}
-                placeholder="You say what?"
+                placeholder={TurnPlaceholder[turnType]}
                 onChange={(e) => setInput(e.target.value)}
               />
 
-              <Button size="icon">
+              <Button size="sm">
                 <Send
                   onClick={async () => {
                     await sendMessage([
                       ...messages,
                       { role: "user", content: input },
                     ]);
-                    setInput("");
+
                     if (inputRef.current) {
                       inputRef.current.value = "";
                     }
